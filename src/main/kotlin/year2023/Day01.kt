df1c05c2ad496f4360250a8f7202813554e8ab4a
Lars Schumann <lars.schumann@measuresforjustice.org>
2023-12-04 22:11:35 -0500

    fixed day 1

--- src/main/kotlin/year2023/Day01.kt
+++ src/main/kotlin/year2023/Day01.kt
@@ -4,27 +4,27 @@ import util.asResourceFile
 
 object Day01 {
     private val digits = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+    private val digitsR = digits.map { d -> d.reversed() }
+
+    private fun findFirstDigit(st: String, digits: List<String>): Char {
+        var x = st
+        var whichDigit = -1
+        var minPos = Int.MAX_VALUE
+        digits.forEachIndexed { idx, s ->
+            val pos = x.indexOf(s)
+            if (pos in 0 until minPos) {
+                minPos = pos
+                whichDigit = idx
+            }
+        }
+        if (whichDigit >= 0) x = x.replaceFirst(digits[whichDigit], (1 + whichDigit).toString())
+        return x.first { c -> c.isDigit() }
+    }
 
     private fun findNumber(st: String): Int {
-        var x = st
-        var notDone = true
-        while (notDone) {
-            var minIdx = -1
-            var minPos = Int.MAX_VALUE
-            digits.forEachIndexed { idx, s ->
-                val pos = x.indexOf(s)
-                if (pos in 0 until minPos) {
-                    minPos = pos
-                    minIdx = idx
-                }
-            }
-            if (minIdx >= 0) x = x.replaceFirst(digits[minIdx], (1 + minIdx).toString()) else notDone = false
-        }
-
-        val y = x.filter { c -> c.isDigit() }
-        val z = "${y.first()}${y.last()}".toInt()
-        println("$st\t$x\t$y\t$z")
-        return z
+        val xFirst = findFirstDigit(st, digits)
+        val xLast = findFirstDigit(st.reversed(), digitsR)
+        return "$xFirst$xLast".toInt()
     }
 
     @JvmStatic
